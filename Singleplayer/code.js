46df76bc749686fae53eb1e077a963e921157af8
theangrybagel <34526427+theangrybagel@users.noreply.github.com>
2020-05-23 15:24:03 -0700

    Collision works

--- Singleplayer/code.js
+++ Singleplayer/code.js
@@ -34,7 +34,10 @@ function AddVs(v1, v2)
 {
 	return new V(v1.x + v2.x, v1.y + v2.y);
 }
-
+function CompareVs(v1, v2)
+{
+	return v1.x == v2.x && v1.y == v2.y;
+}
 class Color{
 	constructor(r, g, b)
 	{
@@ -65,7 +68,10 @@ class Snake
 	{
 		this.direction = new V(0, -1);//going down
 		this.parts = [];
-		this.parts.push(new Tile(new V(5, 5)));
+		this.pendingDeath = false;
+		this.lastPos = new V(5, 5);//last position of the head
+		this.parts.push(new Tile(new V(5, 5)), new Tile(new V(5, 6)), new Tile(new V(5, 7)));
+		this.collided = false;
 	}
 }
 
@@ -74,16 +80,16 @@ class Snake
 //add borders
 for(var x = 0; x < width; x++)
 {
-	var t = new Tile(new V(x, 0), 1);
+	var t = new Tile(new V(x, 0), 0);
 	t.color = new Color(0, 255, 255);
-	var t2 = new Tile(new V(x, height-1), 1);
+	var t2 = new Tile(new V(x, height-1), 0);
 	t2.color = new Color(0, 255, 255);
 }
 for(var y = 0; y < height; y++)
 {
-	var t = new Tile(new V(0, y), 1);
+	var t = new Tile(new V(0, y), 0);
 	t.color = new Color(0, 255, 255);
-	var t2 = new Tile(new V(width-1, y), 1);
+	var t2 = new Tile(new V(width-1, y), 0);
 	t2.color = new Color(0, 255, 255);
 }
 //add a snake
@@ -96,9 +102,11 @@ function Update()
 	if(ticks >= gameUpdateRate)
 	{
 		ticks = 0;
+		UpdatePositions();
+		CollisionTesting();
 		GameUpdate();
 	}
-	//update the game (visual)
+	//update the game (visual) (collision)
 	ctx.clearRect(0, 0, width*gridSize, height*gridSize);
 	objs.forEach(function(tile)
 	{
@@ -107,29 +115,52 @@ function Update()
 	});
 }
 
-
+function CollisionTesting()
+{
+	objs.forEach(function(tile){
+		if(tile.type == 0)
+		{
+			snakes.forEach(function(snake)
+			{
+				if(CompareVs(snake.parts[0].pos, tile.pos) && tile != snake.parts[0])
+					snake.collided = true;
+			});
+		}
+	});
+}
+function UpdatePositions()
+{
+	snakes.forEach(function(snake){
+		snake.lastPos = snake.parts[0].pos;
+		if(!snake.collided)
+			snake.parts[0].pos = AddVs(snake.parts[0].pos, snake.direction);
+	});
+}
 //All game logic happens here
 function GameUpdate()
 {
 	snakes.forEach(function(snake){
-		var lastPos = snake.parts[0].pos;
-		//console.log(lastPos);
-		snake.parts[0].pos = AddVs(snake.parts[0].pos, snake.direction);
-		//check for collision with world
+		var lastPos = snake.lastPos;
 		snake.parts.forEach(function(part, index){
-			if(index != 0)
+			if(index != 0 && !snake.pendingDeath)
 			{
 				var newlastpos = new V(part.pos.x, part.pos.y);
 				part.pos.x = lastPos.x;
 				part.pos.y = lastPos.y;
 				lastPos = newlastpos;
 			}
+			if(snake.collided)
+			{
+				part.color = new Color(255, 255, 255);
+			}
 		});
+		if(snake.collided)
+			snake.pendingDeath = true;
 	});
 }
 
 
 
 
-setInterval(Update, 500);
+setInterval(Update, 150);
 
