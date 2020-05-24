090326ec1adc1adb0faed96aa7269e07817d26d3
Dan Kurtz <me@dankurtz.net>
2020-05-24 02:43:02 +0000

    added engine primative functions

--- multi.js
+++ multi.js
@@ -3,6 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
 /*
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/Singleplayer/index.html');
@@ -16,6 +17,49 @@ app.get('/engine.js', function(req, res) {
 	res.sendFile(__dirname + '/Singleplayer/engine.js');
 });
 */
+
+class V{
+	constructor(x, y){
+		this.x = x;
+		this.y = y;
+		this.area = function()
+		{
+			return this.x * this.y;
+		}
+	}
+}
+function AddVs(v1, v2)
+{
+	return new V(v1.x + v2.x, v1.y + v2.y);
+}
+function SubVs(v1, v2)
+{
+	return new V(v1.x - v2.x, v1.y - v2.y);
+}
+function CompareVs(v1, v2)
+{
+	return v1.x == v2.x && v1.y == v2.y;
+}
+class Color{
+	constructor(r, g, b)
+	{
+		this.r = r == null ? 0 : r;
+		this.g = g == null ? 0 : g;
+		this.b = b == null ? 0 : b;
+		this.ToString = function()
+		{
+			return "rgb("+this.r+","+this.g+","+this.b+")";
+		}
+	}
+}
+function Rand(min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min));
+}
+
+function RandomChoice(arr)
+{
+	return arr[Rand(0, arr.length)]
+}
 
 class Tile
 {
